c41b240e0ee702822f8b89cd2e14392dde208f87
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-07-27 11:36:45 -0300

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import "./Styles/_index.scss";
+
+import Home from "./Pages/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/App/Pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import styles from "./Home.module.scss";
+
+function Home() {
+  return <div className={styles.App}></div>;
+}
+
+export default Home;
