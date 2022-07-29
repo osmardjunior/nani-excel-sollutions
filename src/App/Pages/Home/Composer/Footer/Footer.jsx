10bb869dd3460cffe388a5763973f99404b87979
misakidrummer <nanimotooka@gmail.com>
2022-07-29 17:24:11 -0300

    fiz isso ai

---
+++ src/App/Pages/Home/Composer/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import style from './Footer.module.scss'
+
+const footer = () => {
+    const { rectangle, image, cards } = style
+}
+
+return (
+    <footer className={image}>
+        
+    <p>
+        s
+    </p>
+
+    </footer>
+
+   
+
+)
+
+
+export default footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
