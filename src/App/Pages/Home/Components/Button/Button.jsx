c9c8eedb68dc8adde63f89219ac9987ed2b092fe
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-08-02 17:26:06 -0300

    feat: buttonlink

--- src/App/Pages/Home/Components/Button/Button.jsx
+++ src/App/Pages/Home/Components/Button/Button.jsx
@@ -1,7 +1,18 @@
 import styles from "./Button.module.scss";
 import { Link } from "react-router-dom";
 
-const Button = ({ title, classes, link }) => {
+const LinkItem = ({ title, classes, link }) => {
+  const buttonClass = styles.button;
+  const linkCheck = link == undefined ? "#" : link;
+
+  return (
+    <a href={linkCheck} className={`${buttonClass} ${classes}`}>
+      {title}
+    </a>
+  );
+};
+
+const LinkPage = ({ title, classes, link }) => {
   const buttonClass = styles.button;
   const linkCheck = link == undefined ? "#" : link;
 
@@ -10,6 +21,18 @@ const Button = ({ title, classes, link }) => {
       {title}
     </Link>
   );
+};
+
+const Button = ({ title, classes, link, page }) => {
+  // const linkType =
+
+  if (page == true) {
+    return <LinkPage title={title} classes={classes} link={link} />;
+  } else {
+    return (
+      <LinkItem title={title} classes={classes} link={link} aria-label="item" />
+    );
+  }
 };
 
 export default Button;
