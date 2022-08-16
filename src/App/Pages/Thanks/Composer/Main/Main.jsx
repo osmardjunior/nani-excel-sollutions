b3ef4e4e5bbffcc23e276c74f5c9a8aa38f703ec
Nathali Motooka <nanimotooka@gmail.com>
2022-08-16 17:41:04 -0300

    arrumei tudo acho

--- src/App/Pages/Thanks/Composer/Main/Main.jsx
+++ src/App/Pages/Thanks/Composer/Main/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
       <TopWrapper
         title="Aula 1"
         dateDay=""
-        dateString="Terça-feira"
+        dateString=""
         blobSide={true}
         isVideo={false} //quando recerber video trocar url e trocar false por true
         url="/assets/video/aguarde/verde.jpg"
@@ -21,7 +21,7 @@ const Main = () => {
       <TopWrapper
         title="Aula 2"
         dateDay=""
-        dateString="Terça-feira"
+        dateString=""
         blobSide={false}
         isVideo={false}
         url="/assets/video/aguarde/verde.jpg"
@@ -29,7 +29,7 @@ const Main = () => {
       <TopWrapper
         title="Aula 3"
         dateDay=""
-        dateString="Terça-feira"
+        dateString=""
         blobSide={true}
         isVideo={false}
         url="/assets/video/aguarde/verde.jpg"
@@ -38,7 +38,7 @@ const Main = () => {
       <TopWrapper
         title="MasterClass"
         dateDay=""
-        dateString="Terça-feira"
+        dateString=""
         blobSide={false}
         isVideo={false}
         url="/assets/video/maisesperada/laranja.jpg"
